65d28cc399ca35d1b03b5ad58f80134567c0b7de
Joshua Powell <joshua@viable.io>
2019-01-20 22:00:15 -0500

    Added hello world for the project

---
+++ src/main.kt
@@ -0,0 +1,11 @@
+//
+//  main.kt
+//  CoordinateConverter
+//
+//  Created by Joshua Powell on 1/20/19.
+//  Copyright © 2019 Joshua Powell. All rights reserved.
+//
+
+fun main(args : Array<String>) {
+    println("Hello, world!")
+}
